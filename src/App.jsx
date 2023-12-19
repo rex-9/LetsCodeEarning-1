9a234ef4af213bf99f44d5f953e27a23d6ad54b9
Nyi Ye Zin <nyiyezin11@gmail.com>
2023-12-19 14:23:20 +0630

    initial setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
-import About from './pages/About';
+import Services from './pages/Services';
+import Button from './components/Button';
 
 function App() {
 
   return (
     <>
      <Navbar />
+     <h1 className=''></h1>
+     <Button variant={'transparent'}>Here are we</Button>
+     <Button variant={'form'}>Here are we</Button>
+     <Button>Here are we</Button>
+     <Button variant={'icon'} size={'icon'}>
+        Here are we
+      </Button>
      <Routes>
         <Route path='/home' element={<Home/>}></Route>
-        <Route path='/about' element={<About/>}></Route>
+        <Route path='/services' element={<Services/>}></Route>
      </Routes>
     </>
   )
